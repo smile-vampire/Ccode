de87f81e07a14ad1bb553b2766b3c15d48c65c66
smile-vampire <yankai6833@qq.com>
2019-09-22 14:31:21 +0800

    在数论中，水仙花数（Narcissistic number）也称为自恋数、自幂数、 阿姆斯壮数或阿姆斯特朗数（Armstrong number），是指一N位数， 其各个数之N次方和等于该数。1234 = 1^4+2^4+3^4+4^4

---
+++ 求sn的前n和/求sn的前n和/求sn前n项和.cpp
@@ -0,0 +1,42 @@
+//
+//#define _CRT_SECURE_NO_WARNINGS 1#include<stdio.h>
+//#include<stdio.h>
+//int main()
+//{
+//	int  a, i,len;
+//	int Sn = 0;
+//	int n = 0;
+//	printf("输入这个数: ");
+//	scanf("%d", &a);
+//	printf("需要求这个数前几项和: ");
+//	scanf("%d", &len);
+//	for (i= 0; i <len; i++)
+//	{
+//		n = n*10 + a;
+//		Sn += n;
+//	}
+//	printf("%d\n", Sn);
+//	return 0;
+//}
+#define _CRT_SECURE_NO_WARNINGS 1#include<stdio.h>
+#include<stdio.h>
+int main()
+{
+	int a = 2;
+	int n = 0;
+	int sum = 0;
+	int tmp=0;
+	int i;
+	scanf("%d", &n);
+	for (i = 0; i < n; i++)
+	{
+		tmp = tmp* 10 + 2;
+		sum += tmp;
+	}
+	printf("%d\n", sum);
+	return 0;
+}
+ 
+
+
+
