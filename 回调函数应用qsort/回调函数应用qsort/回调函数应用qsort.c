ef3fbc206e9e10d93dc4d09eb9d011d2ba2638b6
smile-vampire <yankai6833@qq.com>
2019-10-10 23:29:28 +0800

    回调函数应用qsort

---
+++ 回调函数应用qsort/回调函数应用qsort/回调函数应用qsort.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//void qsort( void *base, size_t num, size_t width, int (__cdecl *compare )(const void *elem1, const void *elem2 ) )º¯ÊýÔ­ÐÍ£»
+int _clpmnt(const void *p1, const void *p2)
+{
+	return *(int*)p1 - *(int*)p2;
+}
+void Show(int *arr, int len)
+{
+	int i = 0;
+	for (i = 0; i < len; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+int main()
+{
+	int arr[] = { 16, 9, 12, 5, 8, 7, 4, 3 };
+	int len = sizeof(arr) / sizeof(arr[0]);
+	qsort(arr, len, sizeof(int), _clpmnt);
+	Show(arr, len);
+	return 0;
+}
