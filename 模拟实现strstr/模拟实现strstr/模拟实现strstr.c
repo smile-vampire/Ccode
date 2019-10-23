e8f8689306515207b6e0b49e48826fe57c38b9c9
smile-vampire <yankai6833@qq.com>
2019-10-23 23:10:45 +0800

    加一种方法

--- 模拟实现strstr/模拟实现strstr/模拟实现strstr.c
+++ 模拟实现strstr/模拟实现strstr/模拟实现strstr.c
@@ -1,9 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 //可能出现的情况
 //hello word
-      //wor
+//wor
 //hello word
-      //word
+//word
 //aaabbcc
 // aab
 #include<stdio.h>
@@ -16,25 +16,46 @@ const char* my_strstr(const char* str1, const char* str2)
 	const char* sub = str2;
 	while (*src)
 	{
-		const char* ret = sub;
 		const char* start = src;
-		while (*src == *sub && *sub != '\0')
+		while (*sub != '\0'&&*src == *sub)
 		{
 			src++;
 			sub++;
 		}
 		if (*sub == '\0')
-		{//找到了匹配
-			return  ret;
+		{
+			return start;
 		}
 		else
-		{//没有匹配  继续往后走；
-			src=start + 1;
+		{
+			src = start + 1;
 			sub = str2;
 		}
 	}
-	return NULL;//没有找到
+	return NULL;
 }
+//	const char* ret = str1;
+//	const char* start1 = ret;
+//	const char* start2 = str2;
+//	while (*start1)
+//	{
+//		while (*start1 && *start2 && *start1==*start2)
+//		{
+//			start1++;
+//			start2++;
+//		}
+//		if (*start2 == '\0')//找到了
+//		{
+//			return ret;
+//		}
+//		else
+//		{
+//			start1 = ++ret;
+//			start2 = str2;
+//		}
+//	}
+//	return NULL;
+//}
 int main()
 {
 	char* p1 = "hello world";
